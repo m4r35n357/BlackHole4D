453966ae4bbbdce4694979dca5edcfc49c793097
Ian Smith <m4r35n357@gmail.com>
2014-10-17 18:22:19 +0100

    simplifications a la python version

--- src/main/java/uk/me/doitto/KerrMotion.java
+++ src/main/java/uk/me/doitto/KerrMotion.java
@@ -19,9 +19,9 @@ public final class KerrMotion {
 	
 	private final double M, a, horizon, mu2, E, Lz, CC, time, step, a2, lmae2; // constants for this spacetime
 	
-	private double r2, ra2, ra, sth, cth, sth2, cth2, sth3, cth3, csth, sigma, delta, P, R, THETA, TH, P2;  // intermediate variables
+	private double r2, ra2, sth, cth, sth2, cth2, sth3, cth3, csth, sigma, delta, P, R, THETA, TH, P2;  // intermediate variables
 	
-	private double tau, t, r, theta, phi, rDot, thetaDot, x, y, z; // coordinates etc.
+	private double tau, t, r, theta, phi, rDot, thDot, x, y, z; // coordinates etc.
 	
 	private Integrator symplectic;
 	
@@ -52,10 +52,9 @@ public final class KerrMotion {
 		lmae2 = (L - a * E) * (L - a * E);
 	}
 
-	private void updateIntermediates (double r, double theta) {
+	private void updateIntermediates () {
 		r2 = r * r;
 		ra2 = r2 + a2;
-		ra = Math.sqrt(ra2);
 		sth = Math.sin(theta);
 		cth = Math.cos(theta);
 		sth2 = sth * sth;
@@ -73,15 +72,14 @@ public final class KerrMotion {
 		R = P * P - delta * P2;
 		TH = a2 * (mu2 - E * E) + Lz * Lz /sth2;
 		THETA = CC - cth2 * TH;
-//		R_THETA = R + THETA;
 	}
 	
 	private double uT () {  // MTW eq.33.32d
-		return - (ra2 * P / delta - a * (a * E * sth2 - Lz));
+		return ra2 * P / delta - a * (a * E * sth2 - Lz);
 	}
 	
 	private double uPh () {  // MTW eq.33.32c
-		return - (a * P / delta - (a * E - Lz / sth2));
+		return a * P / delta - (a * E - Lz / sth2);
 	}
 	
 	private double hR () {
@@ -89,38 +87,36 @@ public final class KerrMotion {
 	}
 	
 	private double hTh () {
-		return 10.0 * Math.log10(Math.abs(thetaDot * thetaDot - THETA) / 2.0);
+		return 10.0 * Math.log10(Math.abs(thDot * thDot - THETA) / 2.0);
 	}
 	
 	private double h () {
-		return 10.0 * Math.log10(Math.abs(rDot * rDot - R) / 2.0 + Math.abs(thetaDot * thetaDot - THETA) / 2.0);
+		return 10.0 * Math.log10(Math.abs(rDot * rDot - R) / 2.0 + Math.abs(thDot * thDot - THETA) / 2.0);
 	}
 	
 	void updateQ (double c) {
 		double cStep = c * step;
-		t += cStep * uT();
-		r += 2.0 * cStep * rDot;
-		theta = (theta + cStep * 2.0 * thetaDot) % TWOPI;
-		phi = (phi - cStep * uPh()) % TWOPI;
-		updateIntermediates(r, theta);
+		t -= cStep * uT();
+		r += cStep * rDot;
+		theta = (theta + cStep * thDot) % TWOPI;
+		phi = (phi + cStep * uPh()) % TWOPI;
+		updateIntermediates();
 	}
 	
 	void updateP (double c) {
 		double cStep = c * step;
-		rDot += cStep * (4.0 * r * E * P - 2.0 * P2 * (r - M) - 2.0 * mu2 * r * delta);
-		thetaDot += cStep * 2.0 * (csth * TH + Lz * Lz * cth3 / sth3);
+		rDot += cStep * (2.0 * r * E * P - P2 * (r - M) - mu2 * r * delta);
+		thDot += cStep * (csth * TH + Lz * Lz * cth3 / sth3);
 	}
 	
 	public void simulate () {
-		updateIntermediates(r, theta);
-		rDot = (R >= 0.0) ? Math.sqrt(R) : - Math.sqrt(- R);  // MTW eq.33.32b and 33.33c
-		thetaDot = (THETA >= 0.0) ? Math.sqrt(THETA) : - Math.sqrt(- THETA);  // MTW eq.33.32a and 33.33a
+		updateIntermediates();
+		rDot = (R >= 0.0) ? Math.sqrt(R) : -Math.sqrt(-R);  // MTW eq.33.32b and 33.33c
+		thDot = (THETA >= 0.0) ? Math.sqrt(THETA) : -Math.sqrt(-THETA);  // MTW eq.33.32a and 33.33a
 		symplectic.init();
 		do {
-			x = ra * sth * Math.cos(phi);
-			y = ra * sth * Math.sin(phi);
-			z = r * cth;
-			System.out.printf("{\"mino\":%.9e, \"tau\":%.9e, \"H\":%.1f, \"HR\":%.1f, \"HTH\":%.1f, \"t\":%.9e, \"r\":%.9e, \"th\":%.9e, \"ph\":%.9e, \"uT\":%.9e, \"uR\":%.9e, \"uTh\":%.9e, \"uPh\":%.9e, \"x\":%.9e, \"y\":%.9e, \"z\":%.9e}%n", tau, sigma * tau, h(), hR(), hTh(), - t, r, theta, phi, uT(), rDot, thetaDot, uPh(), x, y, z);
+			double ra = Math.sqrt(ra2);
+			System.out.printf("{\"mino\":%.9e, \"tau\":%.9e, \"H\":%.1f, \"HR\":%.1f, \"HTh\":%.1f, \"t\":%.9e, \"r\":%.9e, \"th\":%.9e, \"ph\":%.9e, \"uT\":%.9e, \"uR\":%.9e, \"uTh\":%.9e, \"uPh\":%.9e, \"x\":%.9e, \"y\":%.9e, \"z\":%.9e}%n", tau, sigma * tau, h(), hR(), hTh(), - t, r, theta, phi, uT(), rDot, thDot, uPh(), ra * sth * Math.cos(phi), ra * sth * Math.sin(phi), r * cth);
 			tau += step;
 			symplectic.solve(this);
 		} while (r > horizon && tau <= time);  // outside horizon and in proper time range
