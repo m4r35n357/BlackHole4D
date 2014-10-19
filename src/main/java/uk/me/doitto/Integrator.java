d3eddb52b16b6cc5978ba38a6525f86ceca3abbf
Ian Smith <m4r35n357@gmail.com>
2014-10-19 20:07:27 +0100

    copy integrator coefficients from experimental branch

--- src/main/java/uk/me/doitto/Integrator.java
+++ src/main/java/uk/me/doitto/Integrator.java
@@ -12,8 +12,7 @@ public enum Integrator {
 	STORMER_VERLET_2 {
 		@Override
 		protected void init() {
-			double[] coefficients = new double[1];
-			coefficients[0] = 1.0;
+			double[] coefficients = {1.0};
 			this.coefficients = coefficients;
 		}
 	},
@@ -25,9 +24,7 @@ public enum Integrator {
 		protected void init() {
 			double CUBE_ROOT_2 = Math.pow(2.0, 1.0 / 3.0);
 			double y = 1.0 / (2.0 - CUBE_ROOT_2);
-			double[] coefficients = new double[2];
-			coefficients[0] = y;
-			coefficients[1] = - y * CUBE_ROOT_2;
+			double[] coefficients = {y, - y * CUBE_ROOT_2};
 			this.coefficients = coefficients;
 		}
 	},
@@ -37,11 +34,10 @@ public enum Integrator {
 	STORMER_VERLET_6 {
 		@Override
 		protected void init() {
-			double[] coefficients = new double[4];
-			coefficients[0] = 0.78451361047755726381949763;
-			coefficients[1] = 0.23557321335935813368479318;
-			coefficients[2] = -1.17767998417887100694641568;
-			coefficients[3] = 1.31518632068391121888424973;
+			double[] coefficients = {0.78451361047755726381949763,
+									0.23557321335935813368479318,
+									-1.17767998417887100694641568,
+									1.31518632068391121888424973};
 			this.coefficients = coefficients;
 		}
 	},
@@ -51,15 +47,14 @@ public enum Integrator {
 	STORMER_VERLET_8 {
 		@Override
 		protected void init() {
-			double[] coefficients = new double[8];
-			coefficients[0] = 0.74167036435061295344822780;
-			coefficients[1] = -0.40910082580003159399730010;
-			coefficients[2] = 0.19075471029623837995387626;
-			coefficients[3] = -0.57386247111608226665638773;
-			coefficients[4] = 0.29906418130365592384446354;
-			coefficients[5] = 0.33462491824529818378495798;
-			coefficients[6] = 0.31529309239676659663205666;
-			coefficients[7] = -0.79688793935291635401978884;
+			double[] coefficients = {0.74167036435061295344822780,
+									-0.40910082580003159399730010,
+									0.19075471029623837995387626,
+									-0.57386247111608226665638773,
+									0.29906418130365592384446354,
+									0.33462491824529818378495798,
+									0.31529309239676659663205666,
+									-0.79688793935291635401978884};
 			this.coefficients = coefficients;
 		}
 	},
@@ -69,24 +64,23 @@ public enum Integrator {
 	STORMER_VERLET_10 {
 		@Override
 		protected void init() {
-			double[] coefficients = new double[17];
-			coefficients[0] = 0.09040619368607278492161150;
-			coefficients[1] = 0.53591815953030120213784983;
-			coefficients[2] = 0.35123257547493978187517736;
-			coefficients[3] = -0.31116802097815835426086544;
-			coefficients[4] = -0.52556314194263510431065549;
-			coefficients[5] = 0.14447909410225247647345695;
-			coefficients[6] = 0.02983588609748235818064083;
-			coefficients[7] = 0.17786179923739805133592238;
-			coefficients[8] = 0.09826906939341637652532377;
-			coefficients[9] = 0.46179986210411860873242126;
-			coefficients[10] = -0.33377845599881851314531820;
-			coefficients[11] = 0.07095684836524793621031152;
-			coefficients[12] = 0.23666960070126868771909819;
-			coefficients[13] = -0.49725977950660985445028388;
-			coefficients[14] = -0.30399616617237257346546356;
-			coefficients[15] = 0.05246957188100069574521612;
-			coefficients[16] = 0.44373380805019087955111365;
+			double[] coefficients = {0.09040619368607278492161150,
+									0.53591815953030120213784983,
+									0.35123257547493978187517736,
+									-0.31116802097815835426086544,
+									-0.52556314194263510431065549,
+									0.14447909410225247647345695,
+									0.02983588609748235818064083,
+									0.17786179923739805133592238,
+									0.09826906939341637652532377,
+									0.46179986210411860873242126,
+									-0.33377845599881851314531820,
+									0.07095684836524793621031152,
+									0.23666960070126868771909819,
+									-0.49725977950660985445028388,
+									-0.30399616617237257346546356,
+									0.05246957188100069574521612,
+									0.44373380805019087955111365};
 			this.coefficients = coefficients;
 		}
 	};
