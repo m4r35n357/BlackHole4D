0f557a242b160b3693b25ae4dfcfd208113d122e
Ian Smith <m4r35n357@gmail.com>
2014-10-23 15:43:58 +0100

    do a simulation with newly generated parameters

--- src/main/java/uk/me/doitto/InitialConditions.java
+++ src/main/java/uk/me/doitto/InitialConditions.java
@@ -6,10 +6,10 @@ package uk.me.doitto;
 import static java.lang.Math.PI;
 import static java.lang.Math.cos;
 import static java.lang.Math.sin;
+import static java.lang.Math.sqrt;
 
 import org.apache.commons.math3.linear.Array2DRowRealMatrix;
 import org.apache.commons.math3.linear.ArrayRealVector;
-import org.apache.commons.math3.linear.DecompositionSolver;
 import org.apache.commons.math3.linear.LUDecomposition;
 import org.apache.commons.math3.linear.RealVector;
 
@@ -19,7 +19,7 @@ import org.apache.commons.math3.linear.RealVector;
  */
 public class InitialConditions {
 	
-	private double M = 1.0, mu = 1.0, E = 1.0, L = 2.0, Q = 0.0, a, rMin, rMax, thetaMax;
+	private double M = 1.0, mu = 1.0, E = 1.0, L = 2.0, Q = 0.0, a, rMin, rMax, thetaMin;
 	
 	/**
 	 * 
@@ -27,7 +27,7 @@ public class InitialConditions {
 	public InitialConditions(double rMin, double rMax, double thetaMax, double a) {
 		this.rMin = rMin;
 		this.rMax = rMax;
-		this.thetaMax = thetaMax;
+		this.thetaMin = thetaMax;
 		this.a = a;
 	}
 
@@ -40,7 +40,7 @@ public class InitialConditions {
 	}
 	
 	private RealVector qDot () {
-		return new ArrayRealVector(new double[] { rDot(rMin), rDot(rMax), thetaDot(thetaMax) }, false);
+		return new ArrayRealVector(new double[] { rDot(rMin), rDot(rMax), thetaDot(thetaMin) }, false);
 	}
 	
 	private Array2DRowRealMatrix getJacobian () {
@@ -51,36 +51,26 @@ public class InitialConditions {
 			{ 2.0 * (rMax * rMax + a * a) * (E * (rMax * rMax + a * a) - a * L) + 2.0 * a * (L - a * E) * (rMax * rMax - 2.0 * M * rMax * rMax + a * a),
 				-2.0 * a * (E * (rMax * rMax + a * a) - a * L) - 2.0 * (L - a * E) * (rMax * rMax - 2.0 * M * rMax + a * a),
 				- rMax * rMax + 2.0 * M * rMax - a * a }, 
-			{ 2.0 * cos(thetaMax) * cos(thetaMax) * a * a * E, - (2.0 * cos(thetaMax) * cos(thetaMax) * L) / sin(thetaMax) * sin(thetaMax), 1.0 } }, false);
+			{ 2.0 * cos(thetaMin) * cos(thetaMin) * a * a * E, - 2.0 * cos(thetaMin) * cos(thetaMin) * L / (sin(thetaMin) * sin(thetaMin)), 1.0 } }, false);
 	}
 	
 	private void solve () {
-		RealVector solution = new ArrayRealVector(new double[] { E, L, Q }, false);
-		while (qDot().dotProduct(qDot()) > 1.0e-9) {
-			RealVector newSolution = new LUDecomposition(getJacobian()).getSolver().solve(qDot());
-//			RealVector newSolution = solution.subtract(solver.solve(qDot()));
-//			solution = newSolution;
-			E -= newSolution.getEntry(0);
-			L -= newSolution.getEntry(1);
-			Q -= newSolution.getEntry(2);
-			System.out.println(newSolution);
+		while (qDot().dotProduct(qDot()) > 1.0e-18) {
+			RealVector correction = new LUDecomposition(getJacobian()).getSolver().solve(qDot());
+			E -= correction.getEntry(0);
+			L -= correction.getEntry(1);
+			Q -= correction.getEntry(2);
 		}
-		System.out.println("E: " + E + ", L:" + L + ", Q: " + Q + ", Error = " + qDot().dotProduct(qDot()));
+		System.out.println("E: " + E + ", L: " + L + ", Q: " + Q + ", Error = " + qDot().dotProduct(qDot()));
 	}
 	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-//		RealMatrix jacobian = new Array2DRowRealMatrix(new double[][] {{ 2, 3, -2 }, { -1, 7, 6 }, { 4, -3, -5 } }, false);
-//		DecompositionSolver solver = new LUDecomposition(jacobian).getSolver();
-//
-//		RealVector error = new ArrayRealVector(new double[] { 1, -2, 1 }, false);
-//		RealVector solution = solver.solve(error);
-//
-//		System.out.println(solution);
-		
-		new InitialConditions(3.0, 6.0, PI / 4.0, 1.0).solve();
+		InitialConditions ic = new InitialConditions(3.0, 6.0, PI / 4.0, 1.0);
+		ic.solve();
+		new KerrMotion(1.0, ic.a, 1.0, ic.E, ic.L, ic.Q, sqrt(ic.rMin * ic.rMax), PI / 2.0, 10.0, 0.001, 4).simulate();
 	}
 
 }
